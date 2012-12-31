811ad2f4492554e78aa0f3107c8d246172a10d52
Mike Fullerton <mike@greentongue.com>
2012-12-31 15:37:25 -0800

    working on string building

---
+++ Strings/FLPrettyString.h
@@ -0,0 +1,43 @@
+//
+//  FLPrettyString.h
+//  FishLampCocoa
+//
+//  Created by Mike Fullerton on 12/30/12.
+//  Copyright (c) 2012 Mike Fullerton. All rights reserved.
+//
+#import "FLRequired.h"
+
+#import "FLStringFormatter.h"
+#import "FLWhitespace.h"
+
+
+@interface FLPrettyString : FLStringFormatter<NSCopying, FLStringFormatterDelegate> {
+@private
+    NSMutableString* _string;
+    FLWhitespace* _whitespace;
+    BOOL _needsTabInset;
+    NSInteger _tabIndent;
+    NSString* _eolString;
+}
+
+@property (readonly, assign, nonatomic) NSUInteger length;
+@property (readonly, strong, nonatomic) FLWhitespace* whitespace;
+@property (readwrite, assign, nonatomic) NSInteger tabIndent;
+
+@property (readonly, strong, nonatomic) NSString* string;
+
+- (id) initWithWhitespace:(FLWhitespace*) whitespace;
++ (id) prettyString:(FLWhitespace*) whiteSpace;
+
++ (id) prettyString; // uses default whitespace
+
++ (id) prettyStringWithString:(NSString*) string;
+
+- (void) indent;
+- (void) outdent;
+
+- (void) indent:(void (^)()) block;
+
++ (FLWhitespace*) defaultWhitespace;
+
+@end
---
+++ Strings/FLPrettyString.m
@@ -0,0 +1,137 @@
+//
+//  FLPrettyString.m
+//  FishLampCocoa
+//
+//  Created by Mike Fullerton on 12/30/12.
+//  Copyright (c) 2012 Mike Fullerton. All rights reserved.
+//
+
+#import "FLPrettyString.h"
+
+@interface FLPrettyString ()
+@property (readwrite, strong, nonatomic) NSString* string;
+@property (readwrite, strong, nonatomic) FLWhitespace* whitespace;
+@end
+
+@implementation FLPrettyString
+
+@synthesize string = _string;
+@synthesize whitespace = _whitespace;
+@synthesize tabIndent = _tabIndent;
+
++ (FLWhitespace*) defaultWhitespace {
+    return [FLWhitespace tabbedWithSpacesWhitespace];
+}
+
+- (id) initWithWhitespace:(FLWhitespace*) whitespace {
+    self = [super init];
+    if(self) {
+        self.delegate = self;
+    
+        _string = [[NSMutableString alloc] init];
+        _whitespace = FLRetain(whitespace);
+        _needsTabInset = YES;
+        
+        NSString* eol = _whitespace.eolString;
+        
+        if(FLStringIsNotEmpty(eol)) {
+            _eolString = FLRetain(eol);
+        }
+    }
+    return self;
+}
+
++ (id) prettyString:(FLWhitespace*) whitespace {
+    return FLAutorelease([[[self class] alloc] initWithWhitespace:whitespace]);
+}
+
++ (id) prettyString {
+    return FLAutorelease([[[self class] alloc] initWithWhitespace:[self defaultWhitespace]]);
+}
+
++ (id) prettyStringWithString:(NSString*) string {
+    FLPrettyString* prettyString = FLAutorelease([[[self class] alloc] initWithWhitespace:[self defaultWhitespace]]);
+    [prettyString appendString:string];
+    return prettyString;
+}
+
+- (NSUInteger) length {
+    return _string.length;
+}
+
+#if FL_DEALLOC
+- (void) dealloc {
+    [_eolString release];
+    [_whitespace release];
+    [_string release];
+    [super dealloc];
+}
+#endif
+
+- (void) stringFormatterAppendEOL:(FLStringFormatter*) stringFormatter {
+    if(_eolString) { 
+        [_string appendString:_eolString]; 
+    } 
+    _needsTabInset = YES;
+}
+
+- (void) stringFormatter:(FLStringFormatter*) stringFormatter 
+            appendString:(NSString*) string {
+    
+    if(FLStringIsNotEmpty(string)) {
+
+// only apply inset if the string is not empty
+        if(_needsTabInset) {
+            if(_whitespace) { 
+                [_string appendString:[_whitespace tabStringForScope:self.tabIndent]]; 
+            } 
+            _needsTabInset = NO;
+        }
+        [_string appendString:string];
+    }
+}            
+
+- (NSString*) stringFormatterGetString:(FLStringFormatter*) stringFormatter {
+    return _string;
+}
+
+- (id) copyWithZone:(NSZone*) zone {
+    FLPrettyString* str = [[FLPrettyString alloc] initWithWhitespace:self.whitespace];
+    str.string = FLAutorelease([_string mutableCopy]);
+    str.tabIndent = self.tabIndent;
+    return str;
+}
+
+- (void) indent {
+    ++_tabIndent;
+}
+
+- (void) outdent {
+    --_tabIndent;
+}
+
+- (void) indent:(void (^)()) block {
+    [self endLine];
+    [self indent];
+    block();
+    [self endLine];
+    [self outdent];
+}
+
+@end
+
+@implementation NSObject (FLPrettyString)
+
+- (void) describe:(FLPrettyString*) formatter {
+    [formatter appendLine:[self description]];
+}
+
+- (NSString*) prettyDescription {
+    FLPrettyString* str = [FLPrettyString prettyString];
+    [self describe:str];
+    return [str string];
+}
+
+@end
+
+
